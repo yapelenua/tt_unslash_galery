1b676a02dcdd1d5e45a60781e8464ad90403f3d1
Yaroslav <kbshnik2020@gmail.com>
2023-07-08 14:27:45 +0300

    fixing_deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { HashRouter as Router } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import './index.scss';
 import { App } from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>,
+  document.getElementById('root'),
 );
